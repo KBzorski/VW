9a9b50380014e27fa8956b8f001cd4741245b865
Kamil Bzorski <kamil.bzorski@hotmail.com>
2020-03-22 17:09:25 +0100

    Project Uploaded

---
+++ VW/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace VW
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.Location = new System.Drawing.Point(39, 105);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(704, 315);
+            this.imageBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(126, 44);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 28);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(881, 129);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 64);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1243, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.imageBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ VW/Form1.cs
@@ -0,0 +1,141 @@
+﻿using Emgu.CV;
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//using System.Timers;
+
+using AForge.Video;
+using AForge.Video.DirectShow;
+using Emgu.CV.Structure;
+
+namespace VW
+{
+    public partial class Form1 : Form
+    {
+        Timer timer = new Timer
+        {
+            Interval = 50
+        };
+        
+        VideoCapture capture;
+        public Form1()
+        {
+            InitializeComponent();
+            Run();
+            
+        }
+
+        //CAPTURE IMAGE FROM DEVICE
+        private void Run()
+        {
+            try
+            {
+                capture = new VideoCapture();
+            }
+            catch (Exception Ex)
+            {
+                MessageBox.Show(Ex.Message);
+                return;
+            }
+            Application.Idle += ProcessFrame;
+        }
+
+        //SHOW CAPTURED IMAGE
+        private void ProcessFrame(object sender, EventArgs e)
+        {
+            imageBox1.Image = capture.QueryFrame();
+        }
+
+        
+        //VideoCaptureDevice device;
+
+        //CREATE LIST OF AVAIABLE DEVICES
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FilterInfoCollection filter;
+            filter = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo device in filter)
+                comboBox1.Items.Add(device.Name);
+            try
+            {
+                comboBox1.SelectedIndex = 0;
+            }
+            catch (Exception Ex)
+            {
+                MessageBox.Show("Cannot find any video device");
+                comboBox1.SelectedItem = null;
+                return;
+            }
+            
+            //device = new VideoCaptureDevice();
+        }
+
+        //CHANGE ACTIVE VIDEO DEVICE
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            capture = new VideoCapture(comboBox1.SelectedIndex);
+        }
+
+
+        //FACE DETECTION
+        static readonly CascadeClassifier cascadeClassfier = new CascadeClassifier("haarcascade_frontalface_alt_tree.xml");
+        void detec_face() 
+        {
+
+            Mat frame = new Mat();
+            capture.Retrieve(frame, 0);
+            //imageBox1.Image = frame;
+            Bitmap bitmap = new Bitmap(frame.Bitmap);
+            Image<Bgr, byte> grayImage = new Image<Bgr, byte>(bitmap);
+            Rectangle[] rectangles = cascadeClassfier.DetectMultiScale(grayImage, 1.2, 1);
+            
+            //Bitmap bitmap2 = new Bitmap(bitmap.Width, bitmap.Height);
+            //Graphics background = Graphics.FromImage(bitmap2);
+            //bitmap2.MakeTransparent();
+            //bitmap = bitmap2;
+
+            foreach (Rectangle rectangle in rectangles)
+            {
+                using (Graphics graphics = Graphics.FromImage(bitmap))
+                {
+                    using (Pen pen = new Pen(Color.Red, 1))
+                    {
+                        graphics.DrawRectangle(pen, rectangle);
+                    }
+                }
+            }
+            imageBox1.Image= new Image<Bgr, Byte>(bitmap);
+        }
+
+        private void OnTimerEvent(object sender, EventArgs e)
+        {
+            detec_face();
+        }
+
+        //private void button1_Click(object sender, EventArgs e)
+        //{
+        //    detec_face();
+        //}
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            timer.Enabled = true;
+            timer.Tick += new System.EventHandler(OnTimerEvent);
+            timer.Start();
+        }
+    }
+
+
+        //private void Form1_Load(object sender, EventArgs e)
+        //{
+        //
+        //}
+    
+}
